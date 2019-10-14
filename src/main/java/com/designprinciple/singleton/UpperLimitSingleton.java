45ad3a183c9ae74e5730ebca29c3263a0ccd096b
QingBest <13530121705@139.com>
2019-10-14 08:46:53 +0800

    有上限单例模式

---
+++ src/main/java/com/designprinciple/singleton/UpperLimitSingleton.java
@@ -0,0 +1,35 @@
+package com.designprinciple.singleton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @ClassName UpperLimitSingleton
+ * @Description 有上限的单例，产生固定数量的实例，例如在内存中启动多个reader实例，需要读取文件时就可以快速响应
+ * @User Administrator
+ * @Date 2019/10/14
+ **/
+public class UpperLimitSingleton {
+    //上限数量
+    public static final int maxNumber = 2;
+    //静态实例列表
+    public static ArrayList<UpperLimitSingleton> upperlimitsingleton = new ArrayList<UpperLimitSingleton>();
+    //实例的属性列表
+    public static ArrayList<String> vars = new ArrayList<String>();
+    //初始化所有实例
+    static {
+        for (int i =0 ; i<maxNumber ;i++ ){
+            upperlimitsingleton.add(new UpperLimitSingleton(String.valueOf(i)));
+        }
+    }
+    //添加实例的属性
+    private UpperLimitSingleton( String i ){
+        vars.add(i);
+    }
+
+    public static UpperLimitSingleton getInstance(){
+        Random random = new Random();
+        int number = random.nextInt(maxNumber);
+        return upperlimitsingleton.get(number);
+    }
+}
