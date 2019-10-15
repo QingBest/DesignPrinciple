dc08309b998b70de2ea5f92ad4449ff6e18685ec
QingBest <13530121705@139.com>
2019-10-15 19:43:23 +0800

    主函数

---
+++ src/main/java/com/designprinciple/factory/Main.java
@@ -0,0 +1,15 @@
+package com.designprinciple.factory;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @User Administrator
+ * @Date 2019/10/15
+ **/
+public class Main {
+    public static void main(String[] args){
+        AbstractFactory abstractFactory = new Factory();
+        //只需这里改变产品即可生产不同的产品
+        Product product = abstractFactory.createProduct(ConcreteProduct.class);
+    }
+}
