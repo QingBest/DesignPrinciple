19151c41e2a50559f68f698cf0f5d3315f3fb053
QingBest <13530121705@139.com>
2019-10-15 19:43:52 +0800

    具体产品

---
+++ src/main/java/com/designprinciple/factory/ConcreteProduct.java
@@ -0,0 +1,13 @@
+package com.designprinciple.factory;
+
+/**
+ * @ClassName ConcreteProduct
+ * @Description 具体产品类
+ * @User Administrator
+ * @Date 2019/10/15
+ **/
+public class ConcreteProduct extends Product {
+    public void method2() {
+        //具体产品的操作
+    }
+}
