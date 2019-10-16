dda129c4a3f1c302a72555d3057535b308f3d4ea
QingBest <13530121705@139.com>
2019-10-16 08:51:28 +0800

    具体模板类

--- src/main/java/com/designprinciple/templatemethod/ConcreteTemplate.java
+++ src/main/java/com/designprinciple/templatemethod/ConcreteTemplate.java
@@ -7,7 +7,14 @@ package com.designprinciple.templatemethod;
  * @Date 2019/10/16
  **/
 public class ConcreteTemplate extends AbstractTemplate{
+    @Override
     protected void baseMethod() {
         //业务逻辑
+    }
+
+    @Override
+    protected boolean isTrue() {
+        //钩子方法，重写
+        return false;
     }
 }
