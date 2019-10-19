60e1a8c1cb3ba3440a8ff3a8772688e7dc4b9606
QingBest <13530121705@139.com>
2019-10-19 16:10:59 +0800

    产品类

---
+++ src/main/java/com/designprinciple/builder/Product.java
@@ -0,0 +1,13 @@
+package com.designprinciple.builder;
+
+/**
+ * @ClassName Product
+ * @Description 产品类
+ * @User Administrator
+ * @Date 2019/10/17
+ **/
+public class Product {
+    public void doSomething(){
+        //独立业务
+    }
+}
