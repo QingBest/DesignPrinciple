0f76affd040cb4524190a82910a9c7d6f5de55d9
QingBest <13530121705@139.com>
2019-10-21 08:16:47 +0800

    具体中介类

---
+++ src/main/java/com/designprinciple/mediator/ConcreteMediator.java
@@ -0,0 +1,14 @@
+package com.designprinciple.mediator;
+
+/**
+ * @ClassName ConcreteMediator
+ * @Description 通用中介类
+ * @User Administrator
+ * @Date 2019/10/19
+ **/
+public class ConcreteMediator extends AbstractMediator{
+    public void doSomething() {
+        //调用其他同事的业务逻辑
+        super.concreteColleague.selfMethod();
+    }
+}
