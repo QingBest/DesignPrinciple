070be6e602a544231b4b265eb2e24f3afcdf0244
QingBest <13530121705@139.com>
2019-10-21 08:21:50 +0800

    具体角色

---
+++ src/main/java/com/designprinciple/proxy/RealSubject.java
@@ -0,0 +1,13 @@
+package com.designprinciple.proxy;
+
+/**
+ * @ClassName RealSubject
+ * @Description 具体主题角色（被委托角色，被代理角色）
+ * @User Administrator
+ * @Date 2019/10/17
+ **/
+public class RealSubject extends Subject {
+    public void request() {
+        //业务逻辑处理
+    }
+}
