96a54880c422d168fb5915bf07dbf622cb22095a
QingBest <13530121705@139.com>
2019-10-21 08:22:02 +0800

    抽象角色

---
+++ src/main/java/com/designprinciple/proxy/Subject.java
@@ -0,0 +1,11 @@
+package com.designprinciple.proxy;
+
+/**
+ * @ClassName Subject
+ * @Description 抽象主题角色，可以是抽象类或接口
+ * @User Administrator
+ * @Date 2019/10/17
+ **/
+public abstract class Subject {
+    public abstract void request();
+}
