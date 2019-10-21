2b63d2bf8eceac1d07570fe9ea5aa310b39f4467
QingBest <13530121705@139.com>
2019-10-21 08:15:42 +0800

    抽象同事类

---
+++ src/main/java/com/designprinciple/mediator/AbstractColleague.java
@@ -0,0 +1,15 @@
+package com.designprinciple.mediator;
+
+/**
+ * @ClassName AbstractColleague
+ * @Description 抽象同事类
+ * @User Administrator
+ * @Date 2019/10/19
+ **/
+public abstract class AbstractColleague {
+    protected AbstractMediator mediator;
+
+    public AbstractColleague(AbstractMediator mediator) {
+        this.mediator = mediator;
+    }
+}
