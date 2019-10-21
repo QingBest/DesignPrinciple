120a8e0117f542f736a753d49f1f9d03c31c9cd7
QingBest <13530121705@139.com>
2019-10-21 08:16:26 +0800

    具体同事类

---
+++ src/main/java/com/designprinciple/mediator/ConcreteColleague.java
@@ -0,0 +1,24 @@
+package com.designprinciple.mediator;
+
+/**
+ * @ClassName ConcreteColleague
+ * @Description 具体同事类
+ * @User Administrator
+ * @Date 2019/10/19
+ **/
+public class ConcreteColleague extends AbstractColleague{
+    //传递中介者
+    public ConcreteColleague(AbstractMediator mediator) {
+        super(mediator);
+    }
+    //自有方法
+    public void selfMethod(){
+        System.out.println("selfMethod");
+    }
+    //依赖方法
+    public void depMethod(){
+        //自己不能处理的业务逻辑，委托给中介者处理
+        super.mediator.doSomething();
+    }
+
+}
