d8d86c1e7276770c3680cde4cd156ad3a33fff0b
QingBest <13530121705@139.com>
2019-11-25 10:42:40 +0800

    抽象策略角色

---
+++ src/main/java/com/designprinciple/strategy/Strategy.java
@@ -0,0 +1,11 @@
+package com.designprinciple.strategy;
+
+/**
+ * @ClassName Strategy
+ * @Description 抽象策略角色
+ * @User Administrator
+ * @Date 2019/11/18
+ **/
+public interface Strategy {
+    void doSomething();
+}
