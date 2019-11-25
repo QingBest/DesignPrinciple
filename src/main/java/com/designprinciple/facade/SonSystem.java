7f55a2d39ba3eb822084d80c98d17536766b193c
QingBest <13530121705@139.com>
2019-11-25 10:34:30 +0800

    子系统

---
+++ src/main/java/com/designprinciple/facade/SonSystem.java
@@ -0,0 +1,13 @@
+package com.designprinciple.facade;
+
+/**
+ * @ClassName SonSystem
+ * @Description 子系统
+ * @User Administrator
+ * @Date 2019/11/20
+ **/
+public class SonSystem {
+    public void doSomething(){
+
+    }
+}
