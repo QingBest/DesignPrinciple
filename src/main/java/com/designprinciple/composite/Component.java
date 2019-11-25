3e42f6b7289f4ba2b427fb8ec5b90296c2a4877a
QingBest <13530121705@139.com>
2019-11-25 10:32:14 +0800

    抽象构件

---
+++ src/main/java/com/designprinciple/composite/Component.java
@@ -0,0 +1,14 @@
+package com.designprinciple.composite;
+
+/**
+ * @ClassName Component
+ * @Description 抽象构件
+ * @User Administrator
+ * @Date 2019/11/19
+ **/
+public abstract class Component {
+    //个体和整体都具有的共享
+    public void doSomething(){
+        //业务代码
+    }
+}
