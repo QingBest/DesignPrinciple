38fed6c59248edcac7a7cee889ac7aa2a3565061
QingBest <13530121705@139.com>
2019-11-25 10:27:30 +0800

    目标角色

---
+++ src/main/java/com/designprinciple/adapter/Target.java
@@ -0,0 +1,11 @@
+package com.designprinciple.adapter;
+
+/**
+ * @ClassName Target
+ * @Description 目标角色
+ * @User Administrator
+ * @Date 2019/11/19
+ **/
+public interface Target {
+    void  request();
+}
