76b565d2f0733fb54e8aec808239eb9d8a69a997
QingBest <13530121705@139.com>
2019-11-25 10:33:22 +0800

    抽象构件

---
+++ src/main/java/com/designprinciple/decorator/AbstractComponent.java
@@ -0,0 +1,11 @@
+package com.designprinciple.decorator;
+
+/**
+ * @ClassName AbstractComponent
+ * @Description 抽象构件
+ * @User Administrator
+ * @Date 2019/11/18
+ **/
+public abstract class AbstractComponent {
+    public abstract void operate();
+}
