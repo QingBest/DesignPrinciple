4119ba398422615ce36e47aed90f563c250a24b1
QingBest <13530121705@139.com>
2019-11-25 10:40:45 +0800

    具体处理者2

---
+++ src/main/java/com/designprinciple/response/ConcreteHandler2.java
@@ -0,0 +1,17 @@
+package com.designprinciple.response;
+
+/**
+ * @ClassName ConcreteHandler2
+ * @Description 具体处理者2
+ * @User Administrator
+ * @Date 2019/10/21
+ **/
+public class ConcreteHandler2 extends AbstractHandler{
+    protected Level getHandlerLevel() {
+        return null;
+    }
+
+    protected Response echo(Request request) {
+        return null;
+    }
+}
