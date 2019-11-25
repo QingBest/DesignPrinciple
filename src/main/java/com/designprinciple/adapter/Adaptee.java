3bb05d41165634cb1c5e19623fb66f87db81c68d
QingBest <13530121705@139.com>
2019-11-25 10:25:58 +0800

    源角色，正常服役

---
+++ src/main/java/com/designprinciple/adapter/Adaptee.java
@@ -0,0 +1,13 @@
+package com.designprinciple.adapter;
+
+/**
+ * @ClassName Adaptee
+ * @Description 源角色，正常服役
+ * @User Administrator
+ * @Date 2019/11/19
+ **/
+public class Adaptee {
+    public void doSomething(){
+        System.out.println("源角色，我已经在工作");
+    }
+}
