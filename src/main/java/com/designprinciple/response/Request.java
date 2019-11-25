be47b35539e88dd68dcbc92811c1b92ed63e6a09
QingBest <13530121705@139.com>
2019-11-25 10:41:22 +0800

    请求的等级

---
+++ src/main/java/com/designprinciple/response/Request.java
@@ -0,0 +1,13 @@
+package com.designprinciple.response;
+
+/**
+ * @ClassName Request
+ * @Description 请求的等级
+ * @User Administrator
+ * @Date 2019/10/21
+ **/
+public class Request {
+    public Level getRequestLevel(){
+        return null;
+    }
+}
