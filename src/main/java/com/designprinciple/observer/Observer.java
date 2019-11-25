aa0ed30680d335c69d66fde05b28a41471d64c3f
QingBest <13530121705@139.com>
2019-11-25 10:38:29 +0800

    观察者接口

---
+++ src/main/java/com/designprinciple/observer/Observer.java
@@ -0,0 +1,11 @@
+package com.designprinciple.observer;
+
+/**
+ * @ClassName Observer
+ * @Description 观察者接口
+ * @User Administrator
+ * @Date 2019/11/20
+ **/
+public interface Observer {
+    public void update();
+}
