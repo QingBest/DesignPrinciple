a5978bbef1194a15b33b72193361020b7c3b084d
QingBest <13530121705@139.com>
2019-11-25 10:32:43 +0800

    树叶构件

---
+++ src/main/java/com/designprinciple/composite/Leaf.java
@@ -0,0 +1,15 @@
+package com.designprinciple.composite;
+
+/**
+ * @ClassName Leaf
+ * @Description 树叶构件
+ * @User Administrator
+ * @Date 2019/11/19
+ **/
+public class Leaf extends Component {
+
+    //可以覆写父类方法
+    public void doSomething(){
+
+    }
+}
