6e573f60e56b7e7dc505b75cb6b5c95a942db922
QingBest <13530121705@139.com>
2019-12-05 10:42:40 +0800

    具体实现化角色

---
+++ src/main/java/com/designprinciple/bridge/ConcreteImplementor1.java
@@ -0,0 +1,17 @@
+package com.designprinciple.bridge;
+
+/**
+ * @ClassName ConcreteImplementor1
+ * @Description 具体实现化角色
+ * @User Administrator
+ * @Date 2019/11/26
+ **/
+public class ConcreteImplementor1 implements Implementor {
+    public void doSomething() {
+        //业务逻辑处理
+    }
+
+    public void doAnything() {
+        //业务逻辑处理
+    }
+}
