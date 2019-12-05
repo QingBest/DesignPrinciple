328011449958b37f93b11eab6cdda40dc72de64c
QingBest <13530121705@139.com>
2019-12-05 10:49:29 +0800

    主函数

---
+++ src/main/java/com/designprinciple/state/Main.java
@@ -0,0 +1,18 @@
+package com.designprinciple.state;
+
+/**
+ * @ClassName Main
+ * @Description 主函数
+ * @User Administrator
+ * @Date 2019/11/26
+ **/
+public class Main {
+    public static void main(String[] args){
+        //定义环境角色
+        Context context = new Context();
+        //初始化状态
+        context.setCurrentState(new ConcreteState1());
+        //行为执行
+        context.handle1();
+    }
+}
