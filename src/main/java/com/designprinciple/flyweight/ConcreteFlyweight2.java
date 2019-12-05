7b3e9b36d2ff4b2789875b46607e7229ca0bf1b0
QingBest <13530121705@139.com>
2019-12-05 10:44:59 +0800

    具体享元角色

---
+++ src/main/java/com/designprinciple/flyweight/ConcreteFlyweight2.java
@@ -0,0 +1,17 @@
+package com.designprinciple.flyweight;
+
+/**
+ * @ClassName ConcreteFlyweight2
+ * @Description 具体享元角色
+ * @User Administrator
+ * @Date 2019/11/26
+ **/
+public class ConcreteFlyweight2 extends Flyweight {
+    public ConcreteFlyweight2(String extrinsic) {
+        super(extrinsic);
+    }
+
+    public void operate() {
+
+    }
+}
