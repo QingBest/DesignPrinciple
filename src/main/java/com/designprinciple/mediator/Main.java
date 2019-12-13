992cf3fb18674c90470257b09c5bdf49f5ec64b2
QingBest <13530121705@139.com>
2019-12-13 14:12:11 +0800

    主函数

--- src/main/java/com/designprinciple/mediator/Main.java
+++ src/main/java/com/designprinciple/mediator/Main.java
@@ -2,16 +2,19 @@ package com.designprinciple.mediator;
 
 /**
  * @ClassName Main
- * @Description TODO
+ * @Description 中介模式，不同同事类都通过中介来互相访问，不再是一种蜘蛛网的模式。
  * @User Administrator
  * @Date 2019/10/19
  **/
 public class Main {
     public static void main(String[] args){
+        //创建一个中介
         AbstractMediator mediator = new ConcreteMediator();
+        //创建一个同事,并且注入中介者
         ConcreteColleague colleague = new ConcreteColleague(mediator);
-        //可以有多个同事类，然后通过set注入同事类
-        mediator.setConcreteColleague(colleague);
+        ConcreteColleagueNew newColleague = new ConcreteColleagueNew(mediator);
+        //中介者，注入其他同事类，方便调用
+        mediator.setConcreteColleague(newColleague);
         //完成自己一些事
         colleague.selfMethod();
         //与其他
