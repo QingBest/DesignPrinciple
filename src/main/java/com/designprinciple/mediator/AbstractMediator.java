fabe9608db7b91029c9617cbe54aedb5b20f92d5
QingBest <13530121705@139.com>
2019-12-13 14:11:46 +0800

    抽象中介类

--- src/main/java/com/designprinciple/mediator/AbstractMediator.java
+++ src/main/java/com/designprinciple/mediator/AbstractMediator.java
@@ -8,18 +8,18 @@ package com.designprinciple.mediator;
  **/
 public abstract class AbstractMediator {
     //定义同事类
-    protected ConcreteColleague concreteColleague;
+    protected AbstractColleague Colleague;
 
     /**
      * 采用get/set的方式注入同事类，因为中介中不一定有同事
      */
     //获取同事类
-    public ConcreteColleague getConcreteColleague(){
-        return concreteColleague;
+    public AbstractColleague getConcreteColleague(){
+        return Colleague;
     }
     //设置同事类
-    public void setConcreteColleague(ConcreteColleague concreteColleague){
-        this.concreteColleague = concreteColleague;
+    public void setConcreteColleague(AbstractColleague Colleague){
+        this.Colleague = Colleague;
     }
     //中介模式的业务逻辑处理
     public abstract void doSomething();
